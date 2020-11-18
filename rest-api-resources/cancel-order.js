d7998e3a0ac1af28c983690e85b9155144158fef
Hasan Eyvazoff <hasan@eyvazoff.com>
2020-11-18 22:45:39 +0400

    Update cancel-order.js

--- rest-api-resources/cancel-order.js
+++ rest-api-resources/cancel-order.js
@@ -1,3 +1,3 @@
 export default `
-<p>Order cancellation is possible anytime before the driver has arrived at the pick-up location. After a client has provided a payment for the order the cancellation will lead to cancellation fee.</p>
+<p>Order cancellation is possible anytime before the driver has arrived at the pick-up location. After a client has provided a payment for the order the cancellation will lead to cancellation fee. </p>
 `
